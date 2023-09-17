3e73db7bdd1977c914040040d04684d65ff00be7
mbpawar1990 <mbpawar92@gmail.com>
2023-09-17 17:23:14 +0530

    My second java commit....

---
+++ src/fullpagescreenshots/screenshots1.java
@@ -0,0 +1,13 @@
+package fullpagescreenshots;
+
+public class screenshots1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WedDriverManager.chromedriver().setup();
+		
+		
+
+	}
+
+}
